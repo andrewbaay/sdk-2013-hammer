a49e6d438716cfaf2b7a24df596c4923b0a4c8a8
SCell555 <kubci.rusnk645@gmail.com>
2020-05-25 00:07:30 +0200

    Simulate particle systems in parallel

--- vgui2/matsys_controls/particlepicker.cpp
+++ vgui2/matsys_controls/particlepicker.cpp
@@ -857,20 +857,15 @@ void CParticleSnapshotGrid::SelectSystem( const char *pSystemName, bool bAddToSe
 
 	Repaint();
 }
-/*
+
 static void ProcessPSystem( CParticleSnapshotPanel *&pPanel )
 {
 	pPanel->Simulate();
 }
-*/
+
 void CParticleSnapshotGrid::OnTick()
 {
-//	ParallelProcess( m_Panels.Base(), m_Panels.Count(), ProcessPSystem );
-
-	for ( int i = 0; i < m_Panels.Count(); ++i )
-	{
-		m_Panels[i]->Simulate();
-	}
+	ParallelProcess( "ProcessPSystem", m_Panels.Base(), m_Panels.Count(), ProcessPSystem );
 }
 
 void CParticleSnapshotGrid::OnParticleSystemSelected( const char *SystemName )
