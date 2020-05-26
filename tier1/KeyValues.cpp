c5d8189ccfe2dd752512659ecf0bf0a9bfcfbb80
SCell555 <kubci.rusnk645@gmail.com>
2020-05-26 21:46:32 +0200

    Do not free linked KeyValues

--- tier1/KeyValues.cpp
+++ tier1/KeyValues.cpp
@@ -2370,15 +2370,6 @@ bool KeyValues::LoadFromBuffer( char const *resourceName, CUtlBuffer &buf, IBase
 	} while ( buf.IsValid() );
 
 	AppendIncludedKeys( includedKeys );
-	{
-		// delete included keys!
-		int i;
-		for ( i = includedKeys.Count() - 1; i > 0; i-- )
-		{
-			KeyValues *kv = includedKeys[ i ];
-			kv->deleteThis();
-		}
-	}
 
 	MergeBaseKeys( baseKeys );
 	{
